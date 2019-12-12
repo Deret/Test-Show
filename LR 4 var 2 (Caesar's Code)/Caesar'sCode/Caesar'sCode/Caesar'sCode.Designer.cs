80a65ea493b70dd4ad336f3db68a50da840e781e
Deret <58818067+Deret@users.noreply.github.com>
2019-12-12 18:10:44 +0300

    Add files via upload

---
+++ LR 4 var 2 (Caesar's Code)/Caesar'sCode/Caesar'sCode/Caesar'sCode.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Caesar_sCode
+{
+    partial class Caesar
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(15, 47);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(418, 81);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.textBox1.Location = new System.Drawing.Point(447, 106);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(441, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ключевое слово";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(668, 56);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сдвиг";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DarkRed;
+            this.textBox2.Location = new System.Drawing.Point(673, 106);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(732, 180);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.DarkRed;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(13, 286);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(890, 73);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(732, 224);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1111, 443);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Просмотреть сообщение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DarkRed;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(16, 180);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(709, 73);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 152);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Новый алфавит";
+            // 
+            // Caesar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(916, 390);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Caesar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caesar\'s Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LR 4 var 2 (Caesar's Code)/Caesar'sCode/Caesar'sCode/Caesar'sCode.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caesar_sCode
+{
+    public partial class Caesar : Form
+    {
+        private static string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static char[] newAlpha = new char[36];
+      //  private static string NewAlphavit;
+        public Caesar()
+        {
+            InitializeComponent();
+        }
+        public static string encrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < alpha.Length; i++)
+                {
+                    if (ch == alpha[i])
+                    {
+                        res += newAlpha[i];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+ 
+        public static string decrypt(string Message)
+        {
+            string res = "";
+            foreach (char ch in Message)
+            {
+                for (int i = 0; i < newAlpha.Length; i++)
+                {
+                    if (ch == newAlpha[i])
+                    {
+                        res += alpha[i];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+ 
+        public static void createNewAlpha(string keyWord, int key) // создаёт новый алфавит с помощью ключа
+        {
+            newAlpha = new char[36];
+            bool findSame = false;
+          //  key++;
+            int beg = 0, current = key;
+            // добавить ключевое слово в новый алфавит
+            for (int i = key; i < keyWord.Length + key; i++)
+            {
+                for (int j = key; j < keyWord.Length + key; j++)
+                {
+                    if (keyWord[i - key] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)// если повторений нету, то буква добавляется в новый алфавит
+                {
+                    newAlpha[current] = keyWord[i - key];
+                    current++;
+                }
+                findSame = false;
+            }
+ 
+            // добавить буквы после ключевого слова
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+                if (current == newAlpha.Length)
+                {
+                    beg = i;
+                    break;
+                }
+            }
+ 
+            // добавить буквы после ключевого слова
+            current = 0;
+            for (int i = beg; i < alpha.Length; i++)
+            {
+                for (int j = 0; j < newAlpha.Length; j++)
+                {
+                    if (alpha[i] == newAlpha[j])
+                    {
+                        findSame = true;
+                        break;
+                    }
+                }
+                if (!findSame)
+                {
+                    newAlpha[current] = alpha[i];
+                    current++;
+                }
+                findSame = false;
+            }
+        }
+ 
+        public static string getNewAlpha()
+        {
+            string strNewAlpha = new string(newAlpha);
+            return strNewAlpha;
+        }
+        string open = "",close = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            richTextBox2.Clear();
+            string keyWord = textBox1.Text;
+            int key = int.Parse(textBox2.Text);
+            
+           
+            Caesar.createNewAlpha(keyWord, key);  
+            open = "";
+            close = "";
+            close = Caesar.encrypt(richTextBox1.Text);
+            richTextBox2.AppendText("Зашифрованное: " + close + "\n");
+            richTextBox2.AppendText("");
+            try
+            {
+                richTextBox2.AppendText("1. " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(0).Key.ToString() + " " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(0).Count().ToString() + "\n");
+                richTextBox2.AppendText("2. " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(1).Key.ToString() + " " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(1).Count().ToString() + "\n");
+                richTextBox2.AppendText("3. " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(2).Key.ToString() + " " + close.GroupBy(c => c).OrderByDescending(g => g.Count()).ElementAt(2).Count().ToString() + "\n");
+            }
+            catch
+            {
+                MessageBox.Show("ENTER AT LEAST 3 SYMBOLS");
+            }
+           
+            File.WriteAllText("message.txt", close);
+            textBox3.Clear();
+            for (int i = 0; i < newAlpha.Length; i++)
+            {
+                textBox3.Text +=newAlpha[i];
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            string message = "";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                message = File.ReadAllText(openFileDialog1.FileName);
+            }
+            richTextBox2.AppendText(message);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string close = File.ReadAllText(openFileDialog1.FileName);
+                close = Caesar.decrypt(close);
+                richTextBox2.AppendText("Рассшифрованное: " + close + "\n");
+            }
+        }
+    }
+}
